c924dacdfb1c3f837bbded12c7a74a94165ce194
Travis Gui <maboite41@gmail.com>
2021-02-04 17:11:07 +0100

    importation de l'app

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 //importation du package http de Node,permettant de créer un serveur
 const http = require('http');
+//importation de l'app
+const app = require('./app');
 
 //Serveur prêt
-const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du serveur !');
-});
+const server = http.createServer(app);
 
 //écoute de la requête via un port qui est par def le 3000
 server.listen(process.env.PORT || 3000);
