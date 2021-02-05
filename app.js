422310dbc19e5fcd04b848ad585556cf5eab8dde
Travis Gui <maboite41@gmail.com>
2021-02-05 12:40:20 +0100

    ajout d'un middleware

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const express = require('express');
  la méthode express() permet de créer une app Express*****/
 const app = express();
 
+
+//1) accéder à notre API depuis n'importe quelle origine
+//2)ajouter les headers mentionnés aux requêtes envoyées vers notre API 
+//3)envoyer des requêtes avec les méthodes mentionnées
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 app.use('/api/stuff', (req, res, next) => {
   const stuff = [
     {
