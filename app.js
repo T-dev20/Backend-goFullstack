6ad31be6f41534b4b669b0be9f002ed4fc965a20
Travis Gui <maboite41@gmail.com>
2021-02-06 15:30:23 +0100

    route delete add

--- app.js
+++ app.js
@@ -80,6 +80,12 @@ app.put('/api/stuff/:id', (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
 });
 
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
 /*** Exportation de l'app pour pouvoir y accéder depuis les 
 autres fichiers du projet, notamment notre serveur Node *****/
 module.exports = app;
