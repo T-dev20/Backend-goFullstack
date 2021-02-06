93be27a783112dfef6584254ec34f3d01be3122b
Travis Gui <maboite41@gmail.com>
2021-02-06 16:55:13 +0100

    change

--- app.js
+++ app.js
@@ -66,14 +66,6 @@ app.get('/api/stuff/:id', (req, res, next) => {
     .catch(error => res.status(404).json({ error }));
 });
 
-
-//Requête GET
-app.get('/api/stuff', (req, res, next) => {
-  Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-});
-
 app.put('/api/stuff/:id', (req, res, next) => {
   Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
     .then(() => res.status(200).json({ message: 'Objet modifié !'}))
@@ -83,6 +75,13 @@ app.put('/api/stuff/:id', (req, res, next) => {
 app.delete('/api/stuff/:id', (req, res, next) => {
   Thing.deleteOne({ _id: req.params.id })
     .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+//Requête GET générale
+app.get('/api/stuff', (req, res, next) => {
+  Thing.find()
+    .then(things => res.status(200).json(things))
     .catch(error => res.status(400).json({ error }));
 });
 
