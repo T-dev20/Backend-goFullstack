9d010725b01fe8b8fdcc70443f06836ce27631e9
Travis Gui <maboite41@gmail.com>
2021-02-09 17:39:19 +0100

    fonction modifyThing modifiée

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -39,28 +39,19 @@ exports.getOneThing = (req, res, next) => {
   );
 };
 
+// Dans cette version modifiée de la fonction, on crée un objet thingObject 
+// qui regarde si req.file existe ou non. S'il existe, on traite la nouvelle
+// image ; s'il n'existe pas, on traite simplement l'objet entrant. 
+// On crée ensuite une instance Thing à partir de thingObject , puis on effectue la modification.
 exports.modifyThing = (req, res, next) => {
-  const thing = new Thing({
-    _id: req.params.id,
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId
-  });
-  Thing.updateOne({_id: req.params.id}, thing).then(
-    () => {
-      res.status(201).json({
-        message: 'Thing updated successfully!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
+  const thingObject = req.file ?
+    {
+      ...JSON.parse(req.body.thing),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+  Thing.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
 };
 
 exports.deleteThing = (req, res, next) => {
